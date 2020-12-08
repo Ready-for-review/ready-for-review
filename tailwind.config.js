5761d1e9d7841baa5e2ed60835205bb68e34f583
Daniel Mies <daniel.mies@codecentric.de>
2020-12-08 21:18:40 +0100

    updated dependencies and migrated to tailwind 2.0.

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: colors.blue,
+      gray: colors.gray,
+      indigo: colors.indigo,
+      orange: colors.orange,
+      white: colors.white,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

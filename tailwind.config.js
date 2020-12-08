4ebaf425d22927d331de4bbf0d08286208c44dc9
Daniel Mies <daniel.mies@codecentric.de>
2020-12-08 21:43:41 +0100

    purge configured correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: { mode: "all" },
+  purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {

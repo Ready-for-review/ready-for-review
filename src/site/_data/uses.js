7bb87c56468c83f67b5b82896e767fffbe072736
Sandra Parsick <mail@sandra-parsick.de>
2022-03-25 21:43:05 +0100

    update Sandras Hardware

--- src/site/_data/uses.js
+++ src/site/_data/uses.js
@@ -1,7 +1,91 @@
 module.exports = function () {
   return {
     sandra: {
-      hardware: [],
+      hardware: [
+        {
+          tags: ["notebook"],
+          name: "Lenovo Thinkpad T14 Gen 1 mit Kubuntu",
+          link: "https://www.lenovo.com/de/de/laptops/thinkpad/t-series/ThinkPad-T14-AMD-G1/p/22TPT14T4A2",
+          description: "Solider und schneller Rechner mit AMD Prozessor.",
+        },
+        {
+          tags: ["keyboard", "workspace"],
+          name: "Sharkoon Purewriter RGB mit Khalil Red Switches und US-ANSI-Layout (EURKey-Layout)",
+          link: "https://de.sharkoon.com/product/PWRGB",
+          description:
+            "Meine mechanische Tastatur mit Low Profile und Blinki Blinki.",
+        },
+        {
+          tags: ["keyboard"],
+          name: "Lenovo ThinkPad Bluetooth Keyboard mit TrackPoint",
+          link: "https://support.lenovo.com/us/en/solutions/pd026744-thinkpad-compact-bluetooth-keyboard-with-trackpoint-overview-and-service-parts",
+          description: "Bluetooth Tastatur für unterwegs.",
+        },
+        {
+          tags: ["mouse", "workspace"],
+          name: "Logitech MX Master",
+          link: "https://support.logi.com/hc/de/articles/360025265054--Produktgalerie-MX-Master-Wireless-Mouse",
+          description: "Schöne, ergonomische Bluetooth-Maus.",
+        },
+        {
+          tags: ["mouse"],
+          name: "Logitech MX Anywhere 2S",
+          link: "https://support.logi.com/hc/de/articles/360023300134",
+          description:
+            "Bluetooth Maus für unterwegs, die kein eigenen Dongle braucht.",
+        },
+        {
+          tags: ["webcam"],
+          name: "Papalook Autofokus Webcam 1080P mit Mikrofon, AF925",
+          link: "https://www.papalook.com/products/webcam-1080p-full-hd-papalook-af925-computer-camera-with-microphone-",
+          description: "Gute, günstige Webcam.",
+        },
+        {
+          tags: ["dock", "workspace"],
+          name: "ThinkPad Ultra Dock 135W EU #40AJ0135EU",
+          link: "https://www.lenovo.com/de/de/accessories-and-monitors/top-tech/Ultra-Dock-135-W-EU/p/40AJ0135EU",
+          description:
+            "Überteuerte Dockingstation, die aber auch unter Linux super funktioniert.",
+        },
+        {
+          tags: ["desk", "workspace"],
+          name: "Ikea Bekant",
+          link: "https://www.ikea.com/de/de/p/bekant-schreibtisch-sitz-steh-weiss-s69022537/",
+          description:
+            "Elektronisch höhenverstellbar mit gutem Preis-Leistungsverhältnis.",
+        },
+        {
+          tags: ["chair", "workspace"],
+          name: "Drehstuhl Ikea Markus",
+          link: "https://www.ikea.com/de/de/p/markus-drehstuhl-vissle-dunkelgrau-70261150/",
+          description: "Bequemer Stuhl.",
+        },
+        {
+          tags: ["chair", "workspace"],
+          name: "Fitness-Hocker sedus sefit",
+          link: "https://www.sedus.com/de/produkte/sitzmoebel/sefit",
+          description: "Wenn es aktiver sein soll, sogar höhenverstellbar.",
+        },
+        {
+          tags: ["monitor", "workspace"],
+          name: '2x Dell UltraSharp U2414H (24")',
+          link: "https://www.dell.com/en-us/shop/dell-ultrasharp-24-monitor-u2414h/apd/860-bbcg/monitors-monitor-accessories",
+          description: "Solide Montitore, schon was älter.",
+        },
+        {
+          tags: ["microphone", "podcast"],
+          name: "Blue Yeti mit RADIUS III Dämpfer und Blue Compass Armständer",
+          link: "https://www.bluemic.com/de-de/products/yeti/",
+          description: "USB Microphone mit gutem Preis-Leistungsverhältnis",
+        },
+        {
+          tags: ["headphone", "podcast", "workspace"],
+          name: "Sony WH-1000X M3",
+          link: "https://www.sony.de/electronics/kopfband-kopfhoerer/wh-1000xm3",
+          description:
+            "Super bequeme Noise-Chanceling Kopfhörer, die den Namen verdienen. Wireless via Bluetooth.",
+        },
+      ],
     },
     daniel: {
       hardware: [
@@ -80,7 +164,7 @@ module.exports = function () {
           name: "Zoom H6",
           link: "https://zoomcorp.com/de/de/handy-recorder/handheld-recorders/h6-audio-recorder/",
           description:
-            "War gerade bei \"Herr Mies will's wissen oft im Einsatz.",
+            'War gerade bei "Herr Mies will\'s wissen" oft im Einsatz.',
         },
       ],
     },

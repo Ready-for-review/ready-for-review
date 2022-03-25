ce116dad4b63f247ed06bb76253af309b2b9c26a
Daniel Zenzes <daniel@zenzes.me>
2022-03-25 16:12:27 +0100

    updated profile page for daniel

---
+++ src/site/_data/uses.js
@@ -0,0 +1,85 @@
+module.exports = function () {
+  return {
+    daniel: {
+      hardware: [
+        {
+          tags: ["keyboard", "workspace"],
+          name: "Ducky One 3 SF Daybreak",
+          link: "https://www.duckychannel.com.tw/en/Ducky-One-3-SF-DayBreak",
+          description:
+            "Meine zweite Ducky Tastatur und bisher habe ich den Kauf nicht bereut.",
+        },
+        {
+          tags: ["keyboard"],
+          name: "Logitech K380",
+          link: "https://www.logitech.com/de-de/products/keyboards/k380-multi-device.html",
+          description:
+            "Solide Tastatur, wenn ich mal unterwegs bin, oder auf dem Tablet was schreiben möchte.",
+        },
+        {
+          tags: ["mouse", "workspace"],
+          name: "Logitech MX Master 3",
+          link: "https://www.logitech.com/de-de/products/mice/mx-master-3.910-005695.html",
+          description:
+            "Solide Maus. Gerade der Support für mehrere Geräte gefällt mir und macht die Arbeit leichter.",
+        },
+        {
+          tags: ["dock", "workspace"],
+          name: "CalDigit Thunderbolt Station 4",
+          link: "https://www.caldigit.com/de/thunderbolt-station-4-de/",
+          description:
+            "Gerade, wenn man mehrere Geräte im Einsatz hat absolut nützlich. Für mich mehr als genug Anschlüsse und damit wirklich gut um das Kabelchaos auf dem Schreibtisch zu reduzieren.",
+        },
+        {
+          tags: ["desk", "workspace"],
+          name: "Manchester I",
+          link: "https://www.home24.de/produkt/schreibtisch-manchester-i-akazie-massiv-metall",
+          description: "Schöner, kompakter Schreibtisch.",
+        },
+        {
+          tags: ["notebook"],
+          name: "MacBook Pro (15 Zoll, 2018)",
+          link: "https://support.apple.com/kb/SP776",
+          description: "Läuft seit 4 Jahren problemlos.",
+        },
+
+        {
+          tags: ["webcam"],
+          name: "Logitech StreamCam",
+          link: "https://www.logitech.com/de-de/products/webcams/streamcam.960-001281.html",
+          description:
+            "Kam mit dem Wechsel ins Homeoffice und läuft seitdem problemlos.",
+        },
+        {
+          tags: ["microphone"],
+          name: "Blue Yeti",
+          link: "https://www.bluemic.com/de-de/products/yeti/",
+          description:
+            "War ursprünglich für den Podcast im Einsatz. Da ich diesen inzwischen mit einem Headset aufnehme, nutze ich das Blue Yeti für alle anderen Aufgaben.",
+        },
+
+        {
+          tags: ["chair"],
+          name: "Nitro Concepts E250",
+          link: "https://www.nitro-concepts.de/chairs/e250-gaming-stuhl",
+          description: "Toller Stuhl zum fairen Preis.",
+        },
+
+        {
+          tags: ["headset", "podcast"],
+          name: "Superlux HMC 660",
+
+          description:
+            "Perfekt für den Podcast: niedriger Preis und sehr gute Qualität. Inzwischen kann man wohl problemlos zum Nachfolger greifen.",
+        },
+        {
+          tags: ["recorder", "podcast"],
+          name: "Zoom H6",
+          link: "https://zoomcorp.com/de/de/handy-recorder/handheld-recorders/h6-audio-recorder/",
+          description:
+            "War gerade bei \"Herr Mies will's wissen oft im Einsatz.",
+        },
+      ],
+    },
+  };
+};

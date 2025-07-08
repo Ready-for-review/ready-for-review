16c0872e314a5b4374f5686ae46b3ae5eb773c88
Daniel Zenzes <daniel@zenzes.me>
2025-07-08 12:00:03 +0200

    chore: use tailwind config file to allow using upgrade tool

--- _11ty/postcss.js
+++ _11ty/postcss.js
@@ -1,30 +1,12 @@
-import typography from "@tailwindcss/typography";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import postCss from "postcss";
 import tailwind from "tailwindcss";
-import colors from "tailwindcss/colors.js";
+import tailwindConfig from "../tailwind.config.js";
 
 export const postcss = (cssCode, done) => {
   postCss([
-    tailwind({
-      content: ["./src/**/*.njk", "./src/**/*.md"],
-      darkMode: "media",
-      theme: {
-        colors: {
-          blue: colors.blue,
-          gray: colors.gray,
-          indigo: colors.indigo,
-          orange: colors.orange,
-          white: colors.white,
-          prussian: "#003552",
-          maximumBlue: "#5eb1bf",
-        },
-        extend: {},
-      },
-      safelist: [],
-      plugins: [typography],
-    }),
+    tailwind(tailwindConfig),
     autoprefixer(),
     cssnano({ preset: "default" }),
   ])
